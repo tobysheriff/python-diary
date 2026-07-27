da2b9e4c1c489f716f2fda8bbd06ab07d18b53cb
Toby <tobysh08@gmail.com>
2026-07-27 21:33:50 +0100

    login now returns user data, now I need to implement sessions somehow (im so far out of my depth)

--- login.py
+++ login.py
@@ -1,9 +1,17 @@
-import sqlite3
+import secrets
 import bcrypt
 from variables import Variables
-from data import grab_hash
+from data import grab_hash, find_user
+import os
 users_path = Variables.config.users_db_path
 
-def login(username, password):
-    grab_hash(username)
-    
+def login(username, password:str):
+    hpass = password.encode("utf-8")
+    hash = grab_hash(username)[0]
+    if bcrypt.checkpw(hpass, hash):
+        data = find_user(username)
+        return data
+
+
+if __name__ == "__main__":
+    login("Toby")
