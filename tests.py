73e72ebd2aadbb9d6ebd0461635c110cdf851e4c
Toby <tobysh08@gmail.com>
2026-07-27 21:34:14 +0100

    self explanatory, should write more tests

--- tests.py
+++ tests.py
@@ -1,4 +1,9 @@
 from register import register
+from data import create_db
+from login import login
+from variables import Variables
+
+#create_db(Variables.config.users_db_path)
 
 print(register("Toby", "Pwd987Toby@"))
-
+print("Login: ",login("Toby", "Pwd987Toby@"))
