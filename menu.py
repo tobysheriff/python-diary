1e53498757c09d277e0a89775a37220b959111dc
Toby <tobysh08@gmail.com>
2026-07-29 20:39:04 +0100

    added menu class

--- menu.py
+++ menu.py
@@ -1,7 +1,40 @@
-def menu():
+from user import list_users, find_user
+from entries import get_entries
+
+class Menu:
+    def __init__(self, title, selection):
+        self.title = title
+        self.selection = selection 
+
+    def __str__(self):
+        body = ""
+        for i, v in enumerate(self.selection):
+            body += (f"[{i}] {v}\n")
+        menu = f"--- {self.title} ---\n{body}"
+        return menu
+
+def display_users(users):
+    string = ""
+    for user in users:
+        string += (f"{user[0]}: {user[1]}\n")
+    return string
+
+def user_menu(id, data):
+    user_menu = Menu(
+        f"Options for {data[1]}",
+        ["Create Entry", "View Entries", "Change Password", "Change Username", "Remove Account"]
+    )
+    print(user_menu)
+
+def home_menu():
     menu="""
-    [1] Register
-    [2] Login
-    [3] Exit
-    """
-    
+--- Admin Menu ---
+Select a user
+"""
+    print(menu)
+    print(display_users(list_users()))
+    selectedId = input(">")
+    userData = find_user(selectedId)[1][0]
+    user_menu(selectedId, userData)
+if __name__ == "__main__":
+    home_menu()
